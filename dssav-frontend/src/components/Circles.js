ca7768f759d018180e0c28ed8365c71c38868a60
mhuggi <emil.e.salmi@gmail.com>
2020-09-22 11:23:00 +0300

    example d3

---
+++ dssav-frontend/src/components/Circles.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import useInterval from 'react-useinterval';
+
+
+
+const generateDataset = () => (
+    Array(100).fill(0).map(() => ([
+        Math.random() * 80 + 10,
+        Math.random() * 35 + 10,
+    ]))
+)
+
+const Circles = () => {
+    const [dataset, setDataset] = useState(
+        generateDataset()
+    )
+    useInterval(() => {
+        const newDataset = generateDataset()
+        setDataset(newDataset)
+    }, 2000)
+    return (
+        <svg viewBox="0 0 100 50">
+            {dataset.map(([x, y], i) => (
+                <circle
+                    cx={x}
+                    cy={y}
+                    r="1"
+                />
+            ))}
+        </svg>
+    )
+}
+
+export default Circles
