403b5318ccffbfa051e797d35f9968cd22749c8c
mhuggi <emil.e.salmi@gmail.com>
2020-09-22 11:34:42 +0300

    reformatting

--- dssav-frontend/src/App.js
+++ dssav-frontend/src/App.js
@@ -1,23 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import Boxplot from './components/Boxplot'
-import dataService from './services/data'
 
 function App() {
-  const [dataSet, setDataSet] = useState([])
-  useEffect(() => {
-    dataService.getAll().then(data =>
-      setDataSet( data )
-    )
-  }, [])
-
-  console.log(dataSet)
-
-
 
   return (
-    <div className="App">
       <Boxplot />
-    </div>
   );
 }
 
--- dssav-frontend/src/components/Boxplot.js
+++ dssav-frontend/src/components/Boxplot.js
@@ -1,6 +1,17 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import dataService from '../services/data'
+
 
 const Boxplot = () => {
+    const [dataSet, setDataSet] = useState([])
+    useEffect(() => {
+      dataService.getAll().then(data =>
+        setDataSet( data )
+      )
+    }, [])
+  
+    console.log(dataSet)
+  
 
     return -1
 
